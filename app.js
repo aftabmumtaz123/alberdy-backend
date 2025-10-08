c44bf7ac989b1efe3e319739150b2164d6e930d7
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-10-08 17:23:37 +0500

    root

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.use('/api/offer', require('./router/offer'));
 app.use('/api/orders', require('./router/orderRouter')); // Example: Protected order routes
 app.use('/', require('./router/authRouter')); // Example: Auth routes (login, register, refresh, logout)
 
-app.get("/",req,res)=>{
+app.get("/",(req,res)=>{
   res.json({Message: "API's are running well bro"})
-}
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
