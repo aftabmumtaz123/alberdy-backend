d8bc12b85d55b0bb9aa38926f31979b994ae7421
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-10-20 12:26:42 +0500

    updating configuratioin

--- router/appConfigurationRouter.js
+++ router/appConfigurationRouter.js
@@ -25,7 +25,7 @@ router.post("/set", authMiddleware, upload.single("appLogo"), createAppConfigura
 router.put("/set/:id", authMiddleware, requireRole(['Super Admin', 'Manager']), upload.single("appLogo"), updateAppConfiguration);
 
 // Get configuration (no file upload needed)
-router.get("/get/:id", getAppConfigurationById);
+router.get("/get", getAppConfigurationById);
 
 module.exports = router;
 
