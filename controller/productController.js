29014a84ba1f356c0711aa15db092c34e9c768ec
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-10-21 17:21:56 +0500

    variant

--- controller/productController.js
+++ controller/productController.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises; // For async file cleanup
 const path = require('path');
 
 const Product = require('../model/Product');
-const Variant = require('../model/VariantsProduct');
+const Variant = require('../model/VariantProduct');
 const Category = require('../model/Category');
 const Subcategory = require('../model/subCategory');
 const Brand = require('../model/Brand');
@@ -1067,4 +1067,5 @@ exports.deleteProduct = async (req, res) => {
     res.status(500).json({ success: false, msg: 'Server error deleting product', details: err.message || 'Unknown error' });
   }
 };
+
 
