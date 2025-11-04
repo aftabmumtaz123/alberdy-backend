16d6a474a0710439bd319a69cf64e55f3bec5db3
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-11-04 16:40:10 +0500

    qwerty

--- router/supplierRouter.js
+++ router/supplierRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createSupplier} = require('../controllers/supplierController');
+const {createSupplier} = require('../controller/supplierController');
 
 
 
