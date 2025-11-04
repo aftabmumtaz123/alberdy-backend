c1c21a4491d0dd6599768556f0102cd8b96d6226
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-11-04 16:58:15 +0500

    12345432

--- controller/supplierController.js
+++ controller/supplierController.js
@@ -1,13 +1,41 @@
 const Supplier = require('../model/Suppliar');
 
-
 // Create a new supplier
 exports.createSupplier = async (req, res) => {
-    try {
-        const supplier = new Supplier(req.body);
-        await supplier.save();
-        res.status(201).json(supplier);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const supplier = new Supplier(req.body);
+    await supplier.save();
+
+    res.status(201).json({
+      success: true,
+      message: 'Supplier created successfully',
+      data: supplier,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error creating supplier',
+        error: error.message,
+    });
+};
 }
+
+
+// Get all suppliers
+exports.getAllSuppliers = async (req, res) => {
+  try {
+    const suppliers = await Supplier.find();
+    res.status(200).json({
+        success: true,
+        message: 'Suppliers fetched successfully',
+        data: suppliers,
+    });
+  }
+    catch (error) {
+    res.status(500).json({
+        success: false,
+        message: 'Error fetching suppliers',
+        error: error.message,
+    });
+  }
--- router/supplierRouter.js
+++ router/supplierRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {createSupplier} = require('../controller/supplierController');
+const {createSupplier, getAllSuppliers} = require('../controller/supplierController');
 
 
 
 
 router.post('/', createSupplier);  
+router.get('/', getAllSuppliers);
 
 
 module.exports = router;
