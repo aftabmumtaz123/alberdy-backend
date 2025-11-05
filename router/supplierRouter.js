4de07861d5c8c69e87f4c911d9e3cd830ea32c05
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-11-05 13:27:48 +0500

    12321

--- router/supplierRouter.js
+++ router/supplierRouter.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const {createSupplier, getAllSuppliers, deleteSupplier, updateSupplier, getSupplierById} = require('../controller/supplierController');
+const {
+  createSupplier,
+  getAllSuppliers,
+  deleteSupplier,
+  updateSupplier,
+  getSupplierById,
+} = require('../controller/supplierController');
+const upload = require('../config/multer'); 
 
-
-
-
-router.post('/', createSupplier);  
-router.get('/', getAllSuppliers);
-router.delete('/:id', deleteSupplier);
-router.put('/:id', updateSupplier);
-router.get('/:id', getSupplierById);
+router.post('/', upload.array('attachments', 5), createSupplier); 
+router.get('/', getAllSuppliers); 
+router.get('/:id', getSupplierById); 
+router.put('/:id', upload.array('attachments', 5), updateSupplier); 
+router.delete('/:id', deleteSupplier); 
 
 module.exports = router;
