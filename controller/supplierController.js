026b560357b9e2f3c11178544fa8aad2fb3093c8
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-11-07 11:19:11 +0500

    qwerty

--- controller/supplierController.js
+++ controller/supplierController.js
@@ -126,7 +126,7 @@ exports.createSupplier = async (req, res) => {
 exports.getAllSuppliers = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit;
     const skip = (page - 1) * limit;
 
     const suppliers = await Supplier.find()
