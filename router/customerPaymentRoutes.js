1a5c806c2d4adeb0f75c81aa119e97ab7fc699f2
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-11-13 17:07:54 +0500

    qwertyu

--- router/customerPaymentRoutes.js
+++ router/customerPaymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const customerPaymentController = require('../controller/CustomerPaymentController');
+const customerPaymentController = require('../controller/customerPaymentController');
 const authMiddleware = require('../middleware/auth');
 
 // All routes require authentication
