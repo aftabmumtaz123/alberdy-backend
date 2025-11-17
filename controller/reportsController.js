c8837cf2273c158404bc40f0e0c106e83b359f0e
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-11-17 15:09:53 +0500

    qwerty

--- controller/reportsController.js
+++ controller/reportsController.js
@@ -269,7 +269,7 @@ static async getExpiredProducts(req, res) {
       return {
         productName: v.product?.name || 'Unknown Product',
         category: v.product?.category?.name || 'Uncategorized',
-        image: v.product?.image || 'Image not found',
+        image: v.product?.thumbnail || null,
         expiryDate: expiry.format('YYYY-MM-DD'),
         status,
         statusType,  
