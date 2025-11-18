b6caa2d9e203a9e3dd15c30623541634d4a3f127
Aftab Mumtaz <aftabmumtaz14@gmail.com>
2025-11-18 16:30:41 +0500

    qwerty

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -338,7 +338,7 @@ exports.deletePayment = async (req, res) => {
 // List all payments with filters
 exports.getAllPayments = async (req, res) => {
   try {
-    const { supplier, startDate, endDate, paymentMethod, reference, status, page = 1, limit = 10 } = req.query;
+    const { supplier, startDate, endDate, paymentMethod, reference, status, page = 1, limit } = req.query;
 
     // Build query
     const query = {};
