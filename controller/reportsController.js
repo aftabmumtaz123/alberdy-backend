3dd51b460f42f2757a5ed6fd8b765b3b00214e8b
Aftab Mumtaz <aftabmumtaz14@gmail.com>
2025-11-18 15:57:33 +0500

    qwerty

--- controller/reportsController.js
+++ controller/reportsController.js
@@ -739,7 +739,7 @@ static async getProfitLossReport(req, res) {
         netProfitMargin: `${netMargin.toFixed(2)}%`
       },
       ratios: {
-        costOfGoodsSold: Number(sales.totalCOGS.toFixed(2)),
+        costOfGoodsSold: Number(sales.totalSales.toFixed(2)),
         cogsPercentage: netRevenue > 0 ? `${((sales.totalSales / netRevenue) * 100).toFixed(1)}%` : '12%',
         operatingExpenseRatio: netRevenue > 0 ? `${((totalOperatingExpenses / netRevenue) * 100).toFixed(1)}%` : '0%',
         profitabilityRatio: `${netMargin.toFixed(2)}%`
@@ -755,9 +755,6 @@ static async getProfitLossReport(req, res) {
     res.status(500).json({ success: false, msg: 'Server error', error: error.message });
   }
 }
-
-
-
 
 }
 
