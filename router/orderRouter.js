7b36055dba4e5a5f6e798092d82ca00b92b179fc
Aftab Mumtaz <aftabmumtaz14@gmail.com>
2025-11-26 16:43:42 +0500

    Subscribe

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -502,4 +502,27 @@ router.get('/track/:identifier', async (req, res) => {
   }
 });
 
+
+
+// routes/push.js
+router.post('/subscribe', authMiddleware, requireRole(['Super Admin', 'Manager']), async (req, res) => {
+  const { subscription } = req.body;
+  const PushSubscription = require('../model/PushSubscription');
+
+  await PushSubscription.findOneAndUpdate(
+    { endpoint: subscription.endpoint },
+    {
+      user: req.user.id,
+      endpoint: subscription.endpoint,
+      keys: subscription.keys,
+      role: req.user.role
+    },
+    { upsert: true, new: true }
+  );
+
+  res.json({ success: true, msg: 'Subscribed to notifications' });
+});
+
+
+
 module.exports = router;
